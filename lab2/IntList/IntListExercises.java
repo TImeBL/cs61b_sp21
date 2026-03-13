9a99e4b82f0f6e9a822dbce3a24e862bbc4267ed
TImeBL <xifan81@icloud.com>
2026-03-13 18:38:04 +0800

    done with lab2 really

--- lab2/IntList/IntListExercises.java
+++ lab2/IntList/IntListExercises.java
@@ -52,7 +52,7 @@ public class IntListExercises {
     public static boolean firstDigitEqualsLastDigit(int x) {
         x = Math.abs(x);
         int lastDigit = x % 10;
-        while (x > 10) {
+        while (x >= 10) {
             x = x / 10;
         }
         int firstDigit = x;
